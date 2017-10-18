cf010981e765a103ca67bde962ac5f0519042b53
Aamani6 <lkolla@yahoo.com>
2017-10-17 21:01:49 -0500

    git example1

---
+++ src/test/java/ExampleGit/Test1/Emp2.java
@@ -0,0 +1,23 @@
+package ExampleGit.Test1;
+
+public class Emp2 {
+	
+	int a = 10;
+	int b = 10;
+	
+	public void add(int a, int b) {
+		int c = a+b;
+		System.out.println(c);
+	}
+
+	
+	
+
+	public static void main(String[] args) {
+		
+		Emp2 EM = new Emp2();
+		EM.add(20, 10);
+
+	}
+
+}
